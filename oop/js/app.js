349d635a8ec4ed6c989454bc189fc27fd5b3605b
Miraghaa <miraghaea@code.edu.az>
2023-06-12 08:48:19 +0400

    Homework

---
+++ oop/js/app.js
@@ -0,0 +1,89 @@
+//Dairenin sahesi
+
+// let x = 5 ;
+// let sahe;
+
+// if(typeof x === 'number'){
+//     sahe = Math.PI*(x*x)
+// }else{
+//     alert('Reqem daxil edin')
+// }
+
+// console.log(sahe);
+
+
+// Extends
+
+
+// class Miri{
+//     #adress
+//     constructor(name,surname,age,adress){
+//         this.name = name;
+//         this.surname = surname;
+//         this.age = age;
+//         this.#adress = adress
+//     }
+    
+//     getAdress(){
+//        return this.#adress
+//     } 
+//     setAdress(adress){
+//         return this.#adress=adress
+//     }
+
+// }
+
+
+// let Developer = new Miri('Miraga','Aliyev',23)
+// Developer.setAdress('Capayev')
+// console.log(Developer);
+
+
+// INheritance
+
+
+// class Animal{
+//    constructor(name,surname,age){
+//       this.name=name;
+//       this.surname=surname;
+//       this.age=age;
+//    }
+// }
+
+// class Cat extends Animal{
+//     constructor(name,surname,age,sound){
+//         super(name,surname,age);
+//         this.sound=sound;
+//     }
+
+//     getInfo(){
+//          return `Pisiyin adi: ${this.name} familyasi: ${this.surname} yasi: ${this.age} seisde qeribedi: ${this.sound}`
+//     }
+// }
+
+// let Qarabasan = new Cat('Biz-Biz','Alkasov','Ne vaxt dogulub yalan deyerem','miiiiiyyyyyuuuuuuuu');
+
+
+// console.log(Qarabasan.getInfo());
+
+
+// Array
+
+// let x = [9,8,10,11,5,6,10,3];
+// let small=x[0];
+// let ss;
+
+//     if(Array.isArray(x)){
+//         for(i=0; i<x.length; i++){
+//            x[i]<small
+//            small=x[i]
+//         }
+//     }else{
+//        alert('sagol')
+//     }
+
+// console.log(small);
+
+
+
+
